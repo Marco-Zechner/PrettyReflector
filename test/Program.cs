d911b0567f9a59ea4b2412d4fe5039fab896c9a9
Marco-Zechner <marcoflorian00956@gmail.com>
2025-01-17 18:18:36 +0100

    Added Test Project

---
+++ test/Program.cs
@@ -0,0 +1,12 @@
+﻿using MarcoZechner.PrettyReflector;
+
+namespace MarcoZechner.Test;
+
+public class Program {
+    public static void Main()
+    {
+        (int a, int b, int c, int d, int, int, int, int, int, int) tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        var prettyTuple = Prettify.Variable(() => tuple);
+        Console.WriteLine(prettyTuple);
+    }
+}
